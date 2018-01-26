41b5f5e34c8bd28705e04fb59802cdbe33942723
Chelsea Zhu <qianwei5668@gmail.com>
2018-01-25 20:50:22 -0800

    clock_submission

--- clock.js
+++ clock.js
@@ -1,12 +1,16 @@
+
+
+
  var hourhand = document.getElementById("hour")
  var minutehand = document.getElementById("minute")
  var secondhand = document.getElementById("second")
 
 
  var now = new Date();
- var hour = now.getHours();
- var minute = now.getMinutes();
- var second = now.getSeconds();
+
+ var iniHour = now.getHours() % 12;
+ var iniMinute = now.getMinutes();
+ var iniSecond = now.getSeconds();
 
 
 
@@ -22,17 +26,48 @@ var hourRotation = function (h) {
 	return h * 360 / 12;
 }
 
-var hdegrees = secondRotation(hour);
-var mdegrees = minuteRotation(minute);
-var sdegrees = hourRotation(second); 
+var iniHDegrees = hourRotation(iniHour);
+var iniMDegrees = minuteRotation(iniMinute);
+var iniSDegrees = secondRotation(iniSecond); 
+
+var setInitial = function () {
+	 hourhand.style.transform = "rotate(" + iniHDegrees + "deg)";
+	 minutehand.style.transform = "rotate(" + iniMDegrees + "deg)";
+	 secondhand.style.transform = "rotate(" + iniSDegrees + "deg)";
+};
+
+setInitial();
+
+var hdegrees = 30 / 3600;
+var mdegrees = 6 / 60.0;
+var sdegrees = 6;
+var count = 0;
+
+var myInterval = setInterval(function() {
+ 	 	 hourhand.style.transform = "rotate(" + (iniHDegrees + count * hdegrees) + "deg)";
+		 minutehand.style.transform = "rotate(" + (iniMDegrees + count * mdegrees) + "deg)";
+		 secondhand.style.transform = "rotate(" + (iniSDegrees + count * sdegrees) + "deg)";
+		 
+		 if (count > 60 ) {
+		 	now = new Date();
+		     iniHour = now.getHours() % 12;
+			 iniMinute = now.getMinutes();
+			 iniSecond = now.getSeconds();
+
+			iniHDegrees = hourRotation(iniHour);
+			iniMDegrees = minuteRotation(iniMinute);
+			iniSDegrees = secondRotation(iniSecond); 
 
 
-setInterval(function() {
- 	 hourhand.style.transform = "rotate(" + hdegrees + "deg)";
-	 minutehand.style.transform = "rotate(" + mdegrees + "deg)";
-	 secondhand.style.transform = "rotate(" + sdegrees + "deg)";
-	 console.log("tick");
+			 count = 0;
+		 } 
+		 count += 1;
+	
  }, 1000);
+
+
+
+myInterval;
 
 
 
